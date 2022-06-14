bfa90976d2e7c8e6aa7e09b0b35fa03f8d81bd2b
phil0349 <99641710+phil0349@users.noreply.github.com>
2022-06-14 16:38:56 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
