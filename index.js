c75036c73f3860a47e3bd59f5958bde5dd48d864
phil0349 <99641710+phil0349@users.noreply.github.com>
2022-06-14 23:38:05 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function myFunction() {
-  var x = document.getElementById("myLinks");
+  var x = (document.getElementById("myLinks").style.minWidth = "850px");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
